ace67bbef4d91f0a3288767ade106dba321bfda1
liuqianjing <2758906724@qq.com>
2020-12-17 14:03:07 +0800

    添加自定义图表

---
+++ vue2demo/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <arc-chart></arc-chart>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ArcChart from './components/ArcChart.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    ArcChart
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue2demo/src/components/ArcChart.vue
@@ -0,0 +1,81 @@
+
+<template>
+  <div class="arc-chart" ref="arcChart"></div>
+</template>
+<script>
+import * as Echarts from 'echarts'
+export default {
+  name: "ArcChart",
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {
+      this.init();
+  },
+  methods: {
+      init() {
+          const dom = this.$refs.arcChart;
+          if(!dom) {
+              return false;
+          }
+          console.log(Echarts)
+          const instance = Echarts.init(dom);
+          console.log(instance)
+
+          let option = {
+              graphic: [
+                  {
+                      type: 'arc',
+                      shape: {
+                          cx: 250,
+                          cy: 250,
+                          r: 130,
+                        //   r0: 10,
+                          startAngle: 0,
+                          endAngle: Math.PI * 1,
+                          clockwise: false
+                      },
+                      style: {
+                        //   fill: 'red',
+                          stroke: '#41b883',
+                          lineWidth: 50
+                      }
+                  },
+                  {
+                      type: 'polygon',
+                      shape: {
+                          points: [[110,280], [230, 450], [350, 280]]
+                      },
+                      style: {
+                          fill: '#41b883'
+                      }
+                  },
+                  {
+                      type: 'polygon',
+                      shape: {
+                          points: [[150,280], [230, 400], [310, 280]]
+                      },
+                      style: {
+                          fill: '#35495e'
+                      }
+                  }
+              ]
+          }
+
+          instance.setOption(option)
+      }
+  }
+};
+</script>
+<style lang="css" scoped>
+.arc-chart {
+  width: 500px;
+  height: 500px;
+  background: #fafafa;
+  margin: 0 auto;
+}
+</style>
